f3f075a8e8b999192827c2f270ba1bfb4b76ea33
Aditya Saxena <saxenaadi92@gmail.com>
2021-07-29 19:26:39 +0530

    Added Dashboard Page

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Wrapper from './Dashboard.style';
+
+const Dashboard = () => (
+  <Wrapper>
+    <h1>Dashboard</h1>
+  </Wrapper>
+);
+
+export default Dashboard;
