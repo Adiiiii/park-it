73e0832b1a42a9de698ed2036e00b8f4c8dbdfd1
Aditya Saxena <saxenaadi92@gmail.com>
2021-07-30 16:32:44 +0530

    Added style constants

---
+++ src/constants/style.js
@@ -0,0 +1,19 @@
+export const COLOR = {
+  white: '#fff',
+  yellow: '#e9a80b',
+  black: '#2d2d2d',
+  orange: '#c86004',
+  gray: '#cdcdcd',
+  greenishYellow: '#634c1a',
+
+};
+
+export const FONT_SIZE = {
+  small: '12px',
+  medium: '16px',
+  large: '20px',
+  xl: '24px',
+  xxl: '28px',
+};
+
+export default COLOR;
