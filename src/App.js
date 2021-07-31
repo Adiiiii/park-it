14685737f8eaeb084581e0dbf7f10394889d9535
Aditya Saxena <saxenaadi92@gmail.com>
2021-07-31 22:22:56 +0530

    Integrated react-router in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
