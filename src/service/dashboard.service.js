0cb9793d791ac2ad2b13125c3cb65b50386cbd25
Aditya Saxena <saxenaadi92@gmail.com>
2021-08-01 13:35:39 +0530

    added amenities service

--- src/service/dashboard.service.js
+++ src/service/dashboard.service.js
@@ -3,4 +3,6 @@ import BASE_URL from '../constants/url';
 
 export const getGarageInfo = (id) => axios.get(`${BASE_URL}/garage/${id}`);
 
+export const getGarageAmenities = (id) => axios.get(`${BASE_URL}/garage-amenities/${id}`);
+
 export const getGaragePricing = (id) => axios.get(`${BASE_URL}/pricing/${id}`);
