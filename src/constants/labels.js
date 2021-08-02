980a3c6843bef488eca1dff1518e28aad2d400fe
Aditya Saxena <saxenaadi92@gmail.com>
2021-08-02 10:57:44 +0530

    Updated labels

---
+++ src/constants/labels.js
@@ -0,0 +1,25 @@
+const labels = {
+  SOMETHING_WENT_WRONG: 'Something went wrong, please try again later',
+  CAR_PARKED: 'Car Parked',
+  PARKED_SINCE: 'Parked since',
+  START_PARKING: 'Start Parking',
+  STOP_PARKING: 'Stop Parking',
+  BILL_GENERATED: 'Bill generated',
+  TOTAL_BILL: 'Total Bill',
+  BACK_TO_GARAGE: 'Back to Garage',
+
+};
+
+export const PARKING_STATUS = {
+  PARKED: 'PARKED',
+  NOT_PARKED: 'NOT_PARKED',
+  BILL_GENERATED: 'BILL_GENERATED',
+};
+
+export const DOOR = {
+  ENTRY: 'Entry door',
+  EXIT: 'Exit door',
+  PEDESTERIAN: 'Pedesterian door',
+};
+
+export default labels;
